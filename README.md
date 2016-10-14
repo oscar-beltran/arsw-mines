20989ba38869dfcdf3c590915df512dabcfe4c6e
oscar-beltran <oscar-u2011@otmail.com>
2016-10-14 13:54:49 -0500

    Primer commit

---
+++ README.md
@@ -0,0 +1,6 @@
+### Escuela Colombiana de Ingeniería
+### Arquitecturas de Software - ARSW
+### Proyecto: ¡Juntos contra las minas!
+
+
+Proyecto de la asignatura ARSW de la Escuela Colombiana de Ingeniería Julio Garavito periodo 2016-2
---
+++ src/main/java/edu/eci/arsw/MinesApplication.java
@@ -0,0 +1,12 @@
+package edu.eci.arsw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinesApplication.class, args);
+	}
+}
