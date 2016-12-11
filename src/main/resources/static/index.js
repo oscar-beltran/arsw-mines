b0d8328eb6e51aefb154809b3bf3e65513e15893
deivananderson <deyvananderson@hotmail.com>
2016-12-11 16:00:16 -0500

    Guardando variables de usuario

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -1,12 +1,17 @@
 
+
+/* global Location */
+
 function descrearCampo() {
-    window.location.replace("/Crearcampo.html");
+    window.location.replace("/Crearcampo.html");   
 };
 function desCampoprivado() {
     window.location.replace("/Campoprivado.html");
+
 };
 function desCampopublico() {
     window.location.replace("/Campopublico.html");
+   
 };
 function prueba() {
     window.location.replace("/partida.html");
@@ -16,4 +21,12 @@ function regresar(){
     alert("Saliendo...");
     window.location.replace("/index.html");
 }
+
+function validarNickname(){
+    var x =document.forms["validacion"]["nick"].value;
+    localStorage.setItem("usuario",x);
+    document.getElementById("usuario").innerHTML = localStorage.getItem("usuario");
+}
+
+window.onload(validarNickname());
 
