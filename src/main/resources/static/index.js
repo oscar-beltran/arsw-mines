fc23142356d68b7b23d2eb22fa7beb89950c563c
sergio <sergioeric2006@hotmail.com>
2016-12-13 07:43:20 -0500

    Seleccion de campo publico

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -77,10 +77,54 @@ $(document).ready(function(){
         
 });
 
-$(document).ready(iniciar)(
-            function iniciar(){
-                $("tabla tr td").click(clickTabla);
-            }
-        );
+function myFunction(x)
+{
+if (document.getElementsByClassName("trselected").length > 0) {
+	var element = document.getElementsByClassName("trselected");
+	if (parseInt(element[0].id)%2 !== 0) element[0].className = "impar";
+	else element[0].className = "";
+}
+x.className="trselected";
+}
+
+function myFunction2(evnt)
+{
+var ev = (evnt) ? evnt : event;
+   var code=(ev.which) ? ev.which : event.keyCode;
+   if (code == 40) {
+		if (document.getElementsByClassName("trselected").length > 0) {
+			var element = document.getElementsByClassName("trselected");
+			var num = (parseInt(element[0].id) + 1).toString();
+			myFunction(document.getElementById(num));
+		}
+		else myFunction(document.getElementById(1))
+   }
+   else if (code == 38) {
+		if (document.getElementsByClassName("trselected").length > 0) {
+			var element = document.getElementsByClassName("trselected");
+			var num = (parseInt(element[0].id) - 1).toString();
+			myFunction(document.getElementById(num))
+		}
+		else myFunction(document.getElementById(document.getElementById("tbody").rows.length.toString()))
+    }
+    else if (code == 13 ) {
+        if (document.getElementsByClassName("trselected").length > 0) {
+	    var element = document.getElementsByClassName("trselected");
+	    var info = element[0].cells[0].innerText;
+	    info += " "+element[0].cells[1].innerText;
+	    info += " "+element[0].cells[2].innerText;
+	    info += " "+element[0].cells[3].innerText;
+	    info += " "+element[0].cells[4].innerText;
+	    alert(info);
+	}
+ 
+}
+}
+//Escucha y reacciona el evento cuando se pulsa una tecla
+if (window.document.addEventListener) {
+   window.document.addEventListener("keydown", myFunction2, false);
+} else {
+   window.document.attachEvent("onkeydown", myFunction2);
+}
 
 
