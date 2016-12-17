2d8e84825069a8c73209f3106ad94702b25d3ed5
oscar-beltran <oscar-u2011@hotmail.com>
2016-12-17 09:13:26 -0500

    Editada la clase WebSocketConfig para que haga uso del broker interno en lugar del servidor externo

--- src/main/java/edu/eci/arsw/msgbroker/WebSocketConfig.java
+++ src/main/java/edu/eci/arsw/msgbroker/WebSocketConfig.java
@@ -32,24 +32,16 @@ public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
-        //config.enableSimpleBroker("/topic");
-        config.enableStompBrokerRelay("/topic/").setRelayHost("35.162.214.234").setRelayPort(61613);
-        //config.setApplicationDestinationPrefixes("/app");
-        /*config.enableStompBrokerRelay("/topic/").setRelayHost("zebra.rmq.cloudamqp.com").setRelayPort(61613).
-                setClientLogin("qdzrqwlk").
-                setClientPasscode("cwsIMHxuB2acffGunv3q9t_qkgRxSG4p").
-                setSystemLogin("qdzrqwlk").
-                setSystemPasscode("cwsIMHxuB2acffGunv3q9t_qkgRxSG4p").
-                setVirtualHost("qdzrqwlk");*/
-                
+        config.enableSimpleBroker("/topic");
+        //config.enableStompBrokerRelay("/topic/").setRelayHost("35.162.214.234").setRelayPort(61613);
+                       
         config.setApplicationDestinationPrefixes("/app");
     }
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        //registry.addEndpoint("/stomMines").withSockJS();
-        registry.addEndpoint("/stomMines").setAllowedOrigins("*").withSockJS();
-        //registry.addEndpoint("/stompendpoint").setAllowedOrigins("*").withSockJS();
+        registry.addEndpoint("/stomMines").withSockJS();
+        //registry.addEndpoint("/stomMines").setAllowedOrigins("*").withSockJS();        
     }
 
 }
